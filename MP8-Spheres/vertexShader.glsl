b01bb1ef843f5250e959fa2b3ff21bf047ac0c11
kuock0129 <kuo.ck0129@gmail.com>
2024-11-22 16:31:32 -0800

    Reinstate '[sphere] import core files' commit

---
+++ MP8-Spheres/vertexShader.glsl
@@ -0,0 +1,11 @@
+#version 300 es
+layout(location=0) in vec4 position;
+layout(location=1) in vec4 color;
+out vec4 vColor;
+
+uniform mat4 mv;
+
+void main() {
+    gl_Position = mv * position;
+    vColor = color;
+}
